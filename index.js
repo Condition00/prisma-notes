56d627dab72875defe15aa394dd6069867095c31
Condition00 <anant.kavuru@gmail.com>
2025-06-10 12:57:03 +0530

    meow
    meow meow
    
    Co-Authored-By: Mayank Mahavir <mahavirmayank111@gmail.com>

--- index.js
+++ index.js
@@ -15,3 +15,8 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
