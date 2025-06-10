19e317761631ec424258efa94c681e7306aa86e6
Condition00 <anant.kavuru@gmail.com>
2025-06-10 12:55:29 +0530

    meow

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
