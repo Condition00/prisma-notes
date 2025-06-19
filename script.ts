46755ed04fc195c48d3a0a249f48c1f42517ee60
Condition00 <anant.kavuru@gmail.com>
2025-06-19 22:05:56 +0530

    client operations

--- script.ts
+++ script.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "./generated/prisma";
-const prisma = new PrismaClient();
+const prisma = new PrismaClient(); //{log: ['query']}
 
 async function main() {
     // Clean up all tables in the correct order to avoid constraint violations
@@ -24,7 +24,21 @@ async function main() {
                 select: { id: true},
             },
         },
-    });
+        //createMany: { for bulk insertions, not used here, we cannot use it with nested writes like userPreference.create }
+        //findUnique: { for finding a unique record, not used here } age_name means age and name are unique together
+        //findFirst: { for finding the first record that matches the criteria, not used here
+        //findMany: { for finding multiple records, not used here }
+            //distinct: { for finding distinct records, not used here }
+            //take: 10, // for limiting the number of records returned, not used here
+            //skip: 0, // for skipping a number of records, not used here
+
+        // where: { // for filtering records, not used here
+            // all where findMany queries are:
+            // where: { age: { gt: 18 } }, // for filtering records where age is greater than 18
+            // where: { name: { contains: "John" } }, // for filtering records
+            // where: { email: { startsWith: "meow" } }, // for filtering records where email starts with "meow"
+        });
+
 
     console.log("User created:", user);
 }
